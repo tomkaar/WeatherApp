66a2969d7f607e9d723096cf9b5a54900ee61e16
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-02-25 15:46:34 +0100

    Print each timeSeries Base

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ function map(id, zoom, controls){
 
 
 
-getMyLocation();
+// getMyLocation();
 function getMyLocation() {
   if (navigator.geolocation) {
     loadLocationMessage.classList.add("visible");
@@ -205,91 +205,128 @@ async function loadWeather(lat, lng){
 // Build Weather
 function buildWeather(response){
   console.log(response);
-  console.log(map.getCurrentGeoCode());
+  // console.log(map.getCurrentGeoCode());
 
+  // clear innerhtml to write new content
   today.innerHTML = "";
 
-  var today_date = new Date();
+  // All them varibales
+  let today_date = new Date();
+  let today_temp = getParam("t")[0].values[0];
+  let today_temp_type = getParam("t")[0].unit;
+  let today_type = weatherCodes[ getParam("Wsymb2")[0].values[0] ];
 
-  var today_temp = getParam("t")[0].values[0];
-  var today_temp_type = getParam("t")[0].unit;
-  var today_type = weatherCodes[ getParam("Wsymb2")[0].values[0] ];
+    if(today_temp_type == "Cel"){ today_temp_type = "°"; }
 
-  if(today_temp_type == "Cel"){ today_temp_type = "°"; }
+  let createTemp = new newElement("p", "today_temp", today_temp + today_temp_type);
+  let createType = new newElement("p", "today_type", today_type);
+  let createDate = new newElement("p", "today_date", days[today_date.getDay()] + ", " + today_date.getDate() +  " " + month[today_date.getMonth()] + " " + addZero(today_date.getHours()) + ":" + addZero(today_date.getMinutes()) );
 
-  let createTemp = document.createElement("p");
-    createTemp.innerHTML = today_temp + today_temp_type;
-    createTemp.setAttribute("class", "today_temp");
-
-  let createType = document.createElement("p");
-    createType.innerHTML = today_type;
-    createType.setAttribute("class", "today_type");
-
-  let location = document.createElement("p");
-
-  // if position is found in Google Maps
+  // create location if location is found in google maps
   if(getParamGeo("administrative_area_level_1").length > 0 && getParamGeo("postal_town").length > 0){
     let postal_town = getParamGeo("postal_town")[0].long_name;
     let lan = getParamGeo("administrative_area_level_1")[0].long_name;
-    location.innerHTML = postal_town + ", " + lan;
-    location.setAttribute("class", "today_location");
+    var createLocation = new newElement("p", "today_location", postal_town + ", " + lan);
   } else {
-    location.innerHTML = "Position Undefined.";
-    location.setAttribute("class", "today_location");
+    var createLocation = new newElement("p", "today_location", "Position Undefined.");
   }
 
-  let createDate = document.createElement("p");
-    createDate.innerHTML = days[today_date.getDay()] + ", " + today_date.getDay() +  " " + month[today_date.getMonth()] + " " + addZero(today_date.getHours()) + ":" + addZero(today_date.getMinutes());
-    createDate.setAttribute("class", "today_date");
-
-  let createTypeImg = document.createElement("div");
-    createTypeImg.setAttribute("class", "today_img");
-    let setImage = getImagePosition(getParam("Wsymb2")[0].values[0]);
-    createTypeImg.style.backgroundPosition = setImage.lat + "px " + setImage.lng + "px";
-
-
-  today.append(createTypeImg);
-  today.append(createTemp);
-  today.append(createType);
-  today.append(location);
-  today.append(createDate);
+  // create elements
+  createTemp.create(today);
+  createType.create(today);
+  createLocation.create(today);
+  createDate.create(today);
 
 
 
 
-
-  // Sort timeSeries into arrays after date
+  // Sort timeSeries into array after date + create date array to use a reference
   var timeLength = response.timeSeries.length;
   var allDatesNames = [];
   var sortedDates = [];
 
-  for(var i = 0; i < timeLength - 2; i++) {
+  console.log(timeLength);
+
+  for(var i = 0; i < timeLength; i++) {
     let date = response.timeSeries[i].validTime;
     let dateCut = date.substring(0, 10);
-    
+
     if(dateCut in sortedDates){
-      sortedDates[dateCut].push(response.timeSeries[0]);
+      sortedDates[dateCut].push(response.timeSeries[i]);
     } else{
       allDatesNames.push(dateCut);
       sortedDates[dateCut] = new Array();
-      sortedDates[dateCut].push(response.timeSeries[0]);
+      sortedDates[dateCut].push(response.timeSeries[i]);
     }
   }
 
   console.log(allDatesNames);
   console.log(sortedDates);
 
+  // find each day
+  for (var i = 0; i < allDatesNames.length; i++) {
+    // each day
+    let day = sortedDates[allDatesNames[i]]
+
+    // First of date (top row)
+    let firstOfDay = day[0];
+    let thisDate = new Date(firstOfDay.validTime);
+    console.log(firstOfDay);
+    console.log(thisDate);
+
+    var createRow = document.createElement("div");
+      createRow.setAttribute("class", "EachDate");
+
+    var createTopContainer = document.createElement("div");
+      createTopContainer.setAttribute("class", "EachTopDate");
+
+    var createRowTopDate = document.createElement("p");
+      createRowTopDate.setAttribute("class", "DateTopItem");
+      createRowTopDate.innerHTML = days[thisDate.getDay()] + " " + addZero(thisDate.getDate()) + " " + month[thisDate.getMonth()];
+
+    // Each Item container
+    var createContainer = document.createElement("div");
+      createContainer.setAttribute("class", "EachContainer");
+
+      // Each Item
+      for (var k = 0; k < day.length; k++) {
+        console.log(day[k]);
+        var eachDateItemTime = document.createElement("h4");
+          eachDateItemTime.setAttribute("class", "");
+          eachDateItemTime.innerHTML = "Kl. " + day[k].validTime.substring(11, 16);
+        var eachDateItem = document.createElement("div");
+          eachDateItem.setAttribute("class", "DateItem");
+
+          eachDateItem.appendChild(eachDateItemTime);
+          createContainer.appendChild(eachDateItem);
+      }
+
+    // Append all items
+    createTopContainer.appendChild(createRowTopDate);
+    createRow.appendChild(createTopContainer);
+    createRow.appendChild(createContainer);
+    future.appendChild(createRow);
 
 
 
-  // get SMHI info from parameters
+  }
+
+
+
+
+
+
+
+  // get SMHI weather info from parameters
   function getParam(input){
     return response.timeSeries[0].parameters.filter( filter => filter.name == input);
   }
   function getParamGeo(input) {
     return map.getCurrentGeoCode().address_components.filter(loc => loc.types.includes(input));
   }
+  // add zeros to date while printing
   function addZero(i) { if (i < 10) { i = "0" + i; } return i; }
+  // get position for images from sprite
   function getImagePosition(input){
     if(weather_klart.includes(input)){ return {lat: 2700, lng: 1580} }
     else if(weather_molnigt.includes(input)){ return {lat: 2815, lng: 2040}; }
@@ -299,5 +336,19 @@ function buildWeather(response){
     else if(weather_blixt.includes(input)){ return {lat: 1900, lng: 1685}; }
     else if(weather_hagel.includes(input)){ return {lat: 2125, lng: 1920}; }
     else if(weather_sno.includes(input)){ return {lat: 2245, lng: 1810}; }
+  }
+  // create a new element function
+  function newElement(type, className, content){
+    this.type = type;
+    this.className = className;
+    this.content = content;
+
+    var element = document.createElement(type);
+    element.setAttribute("class", this.className);
+    element.innerHTML = this.content;
+
+    this.create = function(location){
+      location.appendChild(element);
+    }
   }
 }
